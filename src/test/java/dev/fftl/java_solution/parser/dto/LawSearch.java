a81b7f7f278fad4a22e5fc65cf5767125d3c3213
fftl <fftl0785@gmail.com>
2025-04-09 06:02:49 +0900

    add : test dto create

---
+++ src/test/java/dev/fftl/java_solution/parser/dto/LawSearch.java
@@ -0,0 +1,10 @@
+package dev.fftl.java_solution.parser.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class LawSearch {
+    private List<Law> laws;
+}
--- src/test/java/dev/fftl/java_solution/parser/json/JacksonParserTest.java
+++ src/test/java/dev/fftl/java_solution/parser/json/JacksonParserTest.java
@@ -2,40 +2,29 @@ package dev.fftl.java_solution.parser.json;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.fftl.java_solution.parser.Statute;
+import dev.fftl.java_solution.parser.dto.LawSearch;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
-import java.util.*;
+import reactor.core.publisher.Mono;
+
 import java.io.IOException;
 
-
-import static org.junit.jupiter.api.Assertions.*;
-
+@Slf4j
 class JacksonParserTest {
-
-
-    JsonFactory jsonFactory = new JsonFactory();
-    ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     @DisplayName("jackson test")
     void jackson_parse_test() throws Exception {
-        WebClient webClient = WebClient.builder().baseUrl("https://www.law.go.kr/DRF/lawSearch.do?OC=fftl0785&target=eflaw&query=소방&efYd=20250402~20991231&type=json").build();
-        String xmlData = webClient.get().retrieve().bodyToMono(String.class).block();
+        WebClient client = WebClient.builder().baseUrl("http://www.law.go.kr/DRF/lawSearch.do").build();
 
-        objectMapper.readValue(xmlData, Statute.class);
-//        System.out.println(xmlData);
-        JsonParser jsonParser;
-        {
-            try {
-                jsonParser = jsonFactory.createParser(xmlData);
-                System.out.println(jsonParser.nextToken());;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        String str = client.get()
+                .uri("?OC=fftl0785&target=eflaw&type=json&nw=2&query=소방")
+                .retrieve()
+                .bodyToMono(String.class).block();
+
+        log.info("str = {}", str);
     }
 }
